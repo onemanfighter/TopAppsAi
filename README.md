5fa1c2a6518644edf4b18e082a31e595e30285fa
Mr. Robot <mr.robot@Amits-MacBook-Air.local>
2023-12-30 13:58:45 +0530

    BoilerplateCode

---
+++ README.md
@@ -0,0 +1,1 @@
+# Top APPs AI | TopAppsAI.com
---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+/**
+ * Component defintion for the App component.
+ * @returns The App component.
+ */
+function App() {
+  return <div className=" bg-white h-screen w-screen"></div>;
+}
+
+export default App;
